3d76ffa1da41d80c27829b61bd48747e7d621d59
Khuram <khuram.saggu@gmail.com>
2024-12-09 23:24:15 +0500

    application is dockerized

--- e-commerce-back-end/db/config.js
+++ e-commerce-back-end/db/config.js
@@ -1,6 +1,16 @@
-const mongoose= require('mongoose');
-require('dotenv').config()
-mongoose.set("strictQuery", false);
-mongoose.connect(process.env.DATABASE, () => {
-  console.log("Connected to MongoDB");
-});
+const mongoose = require('mongoose');
+
+// Fetch DB_URL from environment variables
+const dbURI = process.env.DB_URL;
+if (!dbURI) {
+  throw new Error('DB_URL environment variable is not defined');
+}
+
+// Connect to MongoDB
+mongoose.connect(dbURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.error('Failed to connect to MongoDB', err));
+
